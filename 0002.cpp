90e51dcd9210beca7b358b771014795f0ed478dd
mrwkeichi <fireaxial@gmail.com>
2021-02-17 20:22:41 +0700

    Create 0002.cpp

---
+++ 0002.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+ int main() {
+	int n; //size for array index
+	cin >> n;
+
+	int index[n]; //index table		
+		for (int i = 0; i < n; i++){
+			cin >> index[i];
+		 }
+
+}
