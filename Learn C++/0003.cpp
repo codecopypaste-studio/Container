14f112355b81d43aa680107682b29fdc972afba2
mrwkeichi <fireaxial@gmail.com>
2021-03-01 20:11:56 +0700

    update evening

--- Learn C++/0003.cpp
+++ Learn C++/0003.cpp
@@ -13,19 +13,27 @@ using namespace std;
 	 if (m > 0 && m <= 100 && n > 0 && n<100) {
 		 	n = n;
 		 	m = m;
-			int mtx[m][n];
+			int mtx1[m][n];
+			int mtx2[m][n];
 
-		//add value to array 
+		//add value to array mtx1
 		for (int im = 0; im < m; im++){
 			for (int in = 0; in < n; in++){
-				cin >> mtx[im][in];
+				cin >> mtx1[im][in];
+			}
+	 	}
+
+		//add value to array mtx2
+		for (int im = 0; im < m; im++){
+			for (int in = 0; in < n; in++){
+				cin >> mtx2[im][in];
 			}
 	 	}
 
 		//print value
 		for (int im = 0; im < m; im++){
 			for (int in = 0; in < n; in++){
-				cout << mtx[im][in] << " ";
+				cout << mtx1[im][in] + mtx2[im][in] << " ";
 			}
 			cout << endl;
 	 	}
