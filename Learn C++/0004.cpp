9f23798ff8f8b3fb698763408bb56ae1898003ee
mrwkeichi <fireaxial@gmail.com>
2021-03-05 19:13:12 +0700

    commit code 0004

--- Learn C++/0004.cpp
+++ Learn C++/0004.cpp
@@ -3,20 +3,42 @@
 using namespace std;
 
  int main() {
-    string va;
+    string input;//input
     string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     string under = "abcdefghijklmnopqrstuvwxyz";
-    cin >> va;
+    bool capital; bool small; //use in decision
+   
+    cin >> input;
 
-   int sva = va.size(); //for counting
+   int sizein = input.size(); //for counting
 
-  //loop(sva) with compare string[] vs array a A to make decision(if else) to print message 
-  //if(sva > 0)
-  for (int i = 0; i < sva; i++){
+  //loop(sizein) with compare string[] vs array a A to make decision(if else) to print message 
+  //if(sizein > 0)
+
+  //expression to check capital size text
+  for (int i = 0; i < sizein; i++){
   	for(int n = 0; n < 26; n++){
-  		if (va[i] == upper[n]){
-  			//expression that decision is return value true for uppercase
-  			}
+  		if (input[i] == upper[n]){
+  			capital = true;}
   	}
   }
+
+   //expression to check small size text
+   for (int i = 0; i < sizein; i++){
+  	   for (int n = 0; n < 26; n++){
+  		   if (input[i] == under[n]){
+  			   small = true;}
+  	}
+  }
+
+   cout << capital << small;
+
+   //Decision output answer
+   if (capital == true && small == false){
+      cout << "All Capital Letter";
+   } else if(capital == false && small == true){
+      cout << "All small Letter";
+   } else {
+      cout << "Mix";
+   }
  }
