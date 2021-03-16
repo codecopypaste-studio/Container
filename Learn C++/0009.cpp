f2aae15a1e0bf69a842813b331694b9eb0200211
mrwkeichi <fireaxial@gmail.com>
2021-03-16 23:30:10 +0700

    uploads

--- Learn C++/0009.cpp
+++ Learn C++/0009.cpp
@@ -1,9 +1,14 @@
 #include <cstdio>
+#include <string>
+
 using namespace std;
 
 int main(){
-    int A, B, C;
-    scanf("%d %d %d", &A, &B, &C);
+    int arr1[3];
+    string arr2[3]
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
 
     return 0;
 }
