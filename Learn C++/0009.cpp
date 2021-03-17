1db0ff3640fcc0656e9e7adebd30b0586defa437
mrwkeichi <fireaxial@gmail.com>
2021-03-17 15:17:31 +0700

    updates

--- Learn C++/0009.cpp
+++ Learn C++/0009.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main(){
     int arr1[3];
-    string arr2["A, B, C"];
+    string arr2[3];
     //int a, b, c;
 
     scanf("%d %d %d", &arr1[0], &arr1[1], &arr1[2]);
-    //scanf("%s%s%s", &arr2[0], &arr2[1], &arr2[2]);
+    scanf("%s%s%s", &arr2[0], &arr2[1], &arr2[2]);
     
      printf("%d %d %d", arr1[0], arr1[1], arr1[2]);
      printf("%s %s %s", arr2[0], arr2[1], arr2[2]);
