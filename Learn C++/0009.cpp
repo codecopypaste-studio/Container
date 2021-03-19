c0148c85995279d9c48366d14dd9bfde1234cdf4
mrwkeichi <fireaxial@gmail.com>
2021-03-20 00:53:58 +0700

    PC2G

--- Learn C++/0009.cpp
+++ Learn C++/0009.cpp
@@ -8,8 +8,11 @@ int main(){
     int num[3];
     char word[3];
 
-//    scanf("%d %d %d", &arr[0][0], &arr[0][1], &arr[0][2]);
-//   scanf("%s%s%s", &arr[1][0], &arr[0][1], &arr[0][2]);
+    scanf("%d %d %d", &num[0], &num[1], &num[2]);
+    scanf("%c%c%c", word[0], word[1], word[2]);
+
+    printf("%d %d %d",num[0], num[1], num[2]);
+    printf("%c%c%c", word[0], word[1], word[2]);
 
 //    for(int im = 0; im < m; im++){ 
 //        for (int in = 0; in < n; in++){
