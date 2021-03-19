6829ec66b8660058574b9b6bac37b5272368cb06
mrwkeichi <fireaxial@gmail.com>
2021-03-19 20:21:45 +0700

    PW2G

--- Learn C++/0009.cpp
+++ Learn C++/0009.cpp
@@ -5,14 +5,8 @@
 using namespace std;
 
 int main(){
-    int v1, v2, v3;
-    string w1,w2,w3;
-    
-    scanf("%d %d %d", &v1, &v2, &v3);
-    scanf("%c %c %c", &w1, &w2, &w3);
-
-    printf("print value: %d %d %d \n", v1, v2, v3);
-    printf("print value: %c %c %c", w1, w2, w3);
+    int num[3];
+    char word[3];
 
 //    scanf("%d %d %d", &arr[0][0], &arr[0][1], &arr[0][2]);
 //   scanf("%s%s%s", &arr[1][0], &arr[0][1], &arr[0][2]);
