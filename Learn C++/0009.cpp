82c89d143d05fb0e3fc9dba8a92305b9037e7cac
mrwkeichi <fireaxial@gmail.com>
2021-03-23 18:05:55 +0700

    PW

--- Learn C++/0009.cpp
+++ Learn C++/0009.cpp
@@ -5,14 +5,14 @@
 using namespace std;
 
 int main(){
-    int a[3];
+    //int a[3];
     char b[3];
 
-    for(int i=0; i < 3; i++) {scanf("%d", &a[0]);}
-    //for(int i=0; i < 3; i++) {scanf("%c", &b[0]);}
+    //for(int i=0; i<3; i++) {scanf("%d", &a[i]);}
+    for(int i=0; i<3; i++) {scanf("%c", &b[i]);}
 
-    for(int i = 0; i < 3; i++){printf("%d",a[i]);}
-    //for(int i = 0; i < 3; i++){printf("%c", b[i]);}
+    //for(int i=0; i<3; i++) {printf("%d", a[i]);}
+    for(int i=0; i<3; i++) {printf("%c", b[i]);}
 
 //    for(int im = 0; im < m; im++){ 
 //        for (int in = 0; in < n; in++){
