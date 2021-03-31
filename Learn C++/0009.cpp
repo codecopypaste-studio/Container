7b51e81a75defde958a92ca8aeb51c34006e0307
mrwkeichi <fireaxial@gmail.com>
2021-03-31 09:05:57 +0700

    update mobile

--- Learn C++/0009.cpp
+++ Learn C++/0009.cpp
@@ -1,23 +1,23 @@
 #include <cstdio>
-//#include <utility>
 #include <string>
 
 using namespace std;
 
 int main(){
-    //int a[3];
-    char b[3];
+    int a[3];
+    char b[4];
 
-    //for(int i=0; i<3; i++) {scanf("%d", &a[i]);}
-    for(int i=0; i<3; i++) {scanf("%c", &b[i]);}
+    for(int i=0; i<3; i++) {scanf("%d", &a[i]);}   
+    for(int f=0; f<3; f++) {scanf("%c", &b[f]);}
 
-    //for(int i=0; i<3; i++) {printf("%d", a[i]);}
-    for(int i=0; i<3; i++) {printf("%c", b[i]);}
+    for(int i=0; i<3; i++) {printf("%d", a[i]);}
+    //for(int i=0; i<3; i++) {printf("%c", b[i]);}
+    printf("%c%c%c", b[0], b[1], b[2]);
 
 //    for(int im = 0; im < m; im++){ 
 //        for (int in = 0; in < n; in++){
 //            scanf("%d %d %d", &)
 //        }
 //    }
-    return 0;
+    //return 0;
 }
