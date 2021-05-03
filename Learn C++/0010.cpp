d687c7996486fedcf9a11ae5b03ab1e8178cfde8
mrwkeichi <fireaxial@gmail.com>
2021-05-03 19:03:33 +0700

    apply file 0010

--- Learn C++/0010.cpp
+++ Learn C++/0010.cpp
@@ -2,9 +2,22 @@
 using namespace std;
 
 int main(){
-	cin >> int input;
-	lengh(input)
-	int box1=0, box2=0, box3=1;
+	//input string
+	string txt;
+	cin >> txt;
+
+	//find lenght string and give it to n
+	int n = txt.length();
+
+	//made box value give it 1 0 0 cuz trickster always place an object at box1 first 
+	int box1=1, box2=0, box3=0;
+
+	//run swap position trough n time by type of string that's read.
+	for(int i=0; i<n; i++){
+		if(txt=="A"){
+			
+		}
+	}
 	
 	//formation a
 	
