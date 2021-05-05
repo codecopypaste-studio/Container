e11d198a1a55365825d1b4efc3aca42e0598580a
mrwkeichi <fireaxial@gmail.com>
2021-05-05 19:43:30 +0700

    update file 0011

--- Learn C++/0011.cpp
+++ Learn C++/0011.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
 	//input integer wih 1 line 1 value
-	//modulo input with 42 and checkout 
+	//modulo input with 42 
+	//conpare unmatch fraction value and counting the number as output
 }
