ca5a335f60a7d653307fa9769899d5e22f7d2798
mrwkeichi <fireaxial@gmail.com>
2021-06-17 11:21:34 +0700

    latest

--- Learn C++/0011.cpp
+++ Learn C++/0011.cpp
@@ -11,11 +11,34 @@ int main(){
 	//modulo input with 42
 	int modulo[10];
 	for(int i=0; i<10; i++){
-		 modulo[i] = input[i]%42;
+		 modulo[i] = input[i]%3;
 	}
-	//conpare unmatch fraction value and counting the number as output
-	for(int i=0; i<1000; i++){
-		for(int n=0; n<10; n++){
-		if(modulo[n]==i){}
-	}}
+	//compare unmatch fraction value and counting the number as output
+	//find differnce of i0 - i9 (10 position)
+	int V[10];
+	int a=0;
+	for(int i=0; i<10; i++){
+		if(i==0){V[a] = modulo[i]; a=a+1;} //set first value for V-array
+		
+		/*set value to next v-array*/
+		for(int n=i+1; n<10; n++){
+			if(modulo[n]!=modulo[i]){
+				V[a]=modulo[n];
+				a=a+1;
+				}
+		}
+		
+
+//			for(int n=i+1; n<10; n++){
+//				if(modulo[i]==modulo[n]){}}
+	}
+	for(int i = 0; i<10; i++){
+	cout << modulo[i] << ", ";
+	}
+	
+	cout << "\n";
+	
+	for(int i=0; i<10; i++){
+	cout << V[i] << ", ";
+	}
 }
