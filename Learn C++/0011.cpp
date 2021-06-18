73098b74023f51d66e5080cf5f10d868e68ec887
mrwkeichi <fireaxial@gmail.com>
2021-06-18 18:48:20 +0700

    pass files

--- Learn C++/0011.cpp
+++ Learn C++/0011.cpp
@@ -3,42 +3,59 @@ using namespace std;
 
 int main(){
 	//input integer wih 1 line 1 value
-	int input[3];
-	for (int i=0; i<3; i++){
+	int input[10];
+	for (int i=0; i<10; i++){
 		cin >> input[i];
 	}
 
 	//modulo input with 42
-	int mod[3];
-	for(int i=0; i<3; i++){
+	int mod[10];
+	for(int i=0; i<10; i++){
 		 mod[i] = input[i] % 3;
 	}
 	//compare unmatch fraction value and counting the number as output
 	//find differnce of i0 - i9 (10 position)
-	int sort[3];
+	int sort[10];
 	int a=0;
+	
 	for(int i=0; i<3; i++){
-		if(i==0){sort[a] = mod[i]; a=a+1;} //set first value for V-array
+		if(i==0){
+			sort[a]=mod[i];
+			a++;
+		}
+		else
+			for(int n=i+1; n<10; n++)
+	}
+
+
+/*
+	//set first value for V-array
+	for(int i=0; i<3; i++){
+		if(i==0){
+			sort[a]=mod[i];
+			a=a+1;
+			} 
 		
-		/*set value to next v-array*/
+		/*set value to next v-array*
 		for(int n=i+1; n<3; n++){
 			if(mod[n]!=mod[i]){
 				sort[a]=mod[n];
 				a=a+1;
 				}
 		}
-		
-
+	
 //			for(int n=i+1; n<10; n++){
 //				if(modulo[i]==modulo[n]){}}
 	}
-	for(int i = 0; i<3; i++){
+*/
+
+	for(int i = 0; i<10; i++){
 	cout << input[i] << " ";
 	}
 	
 	cout << "\n";
 
-	for(int i = 0; i<3; i++){
+	for(int i = 0; i<10; i++){
 	cout << mod[i] << " ";
 	}
 	
